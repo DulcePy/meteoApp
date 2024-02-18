a9d2cd10c00c497d1d7f8e95cefdbdfe443dc072
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2024-02-18 16:02:08 -0300

    added api

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "9234dd59ca8092tfabb2o8078c5f4477";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
